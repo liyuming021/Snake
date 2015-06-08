ba9582961889fc88698f8a78ea8b8faf3293fa23
liyuming021 <liyuming021@163.com>
2015-06-08 17:51:22 +0800

    改了蛇的长度和颜色为属性，更灵活，更面向对象

--- src/com/mrk/snake/controller/Controllor.java
+++ src/com/mrk/snake/controller/Controllor.java
@@ -6,15 +6,20 @@ import com.mrk.snake.view.GamePanel;
 public class Controllor {
 
 	private Snake snake;
-	private GamePanel gamePanel;	
+	private GamePanel gamePanel;
+	
 	public Controllor(Snake snake, GamePanel gamePanel) {
 		super();
 		this.snake = snake;
 		this.gamePanel = gamePanel;
 	}
+	/**
+	 * ∆Ù∂Ø”Œœ∑
+	 */
 	public void newGame() {
 		gamePanel.redisplay(snake);
 	}
+	
 	public GamePanel getGamePanel() {
 		return gamePanel;
 	}
--- src/com/mrk/snake/model/Snake.java
+++ src/com/mrk/snake/model/Snake.java
@@ -10,7 +10,13 @@ import com.mrk.snake.util.Global;
 public class Snake {
 	//蛇身，由一组点构成
 	private LinkedList<Point> body = new LinkedList<Point>();
-
+	//蛇的长度，默认设置为4
+	private int length = 4;
+	//蛇的颜色，默认为红色
+	private Color color = Color.red;
+	/**
+	 * 构造蛇，尾巴在面板中心，头在右边，默认为4个格子长度，红色
+	 */
 	public Snake() {
 		init();
 	}
@@ -21,7 +27,7 @@ public class Snake {
 		int x = Global.WIDTH/2;
 		int y = Global.HEIGHT/2;
 		//四个点长度，头在右边
-		for(int i=0; i<4; i++) {
+		for(int i=0; i<length; i++) {
 			body.addFirst(new Point(x++,y));
 		}
 	}
@@ -30,10 +36,10 @@ public class Snake {
 	 * @param g 接受Graphics对象
 	 */
 	public void drawMe(Graphics g) {
-		//每一格身体上的点
+		//蛇身体上的每一个点
 		for(Point p : body) {
 			//画什么颜色
-			g.setColor(Color.red);
+			g.setColor(color);
 			//怎么画，已点为基准，往下画高度，往右画宽度，形成矩形
 			g.fill3DRect(p.x * Global.CELL_WIDTH, p.y * Global.CELL_HEIGHT, 
 					Global.CELL_WIDTH, Global.CELL_HEIGHT, true);
